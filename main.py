91783b31af831c9e8ee92a610f29cd79d48fe2f2
yeyevtushenko <yehor803@gmail.com>
2024-01-04 15:04:18 +0200

    програма створює та експортує дані у форматі JSON

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import json
 
+orders = []
+while True:
+    id = int(input("Введіть ID замовлення (або 0 для завершення введення): "))
+    if id == 0:
+        break
+
+    product = input("Введіть назву товару: ")
+    quantity = int(input("Введіть кількість: "))
+
+    order = {"order_id": id, "product": product, "quantity": quantity}
+    orders.append(order)
+
+
+with open("orders.json", "w") as json_file:
+    json.dump(orders, json_file)
+
+print("Дані успішно експортовано.")
